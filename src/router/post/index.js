cfc55c7771a36c0e8e33253b0747d8e3f55b6bca
Shameel Ahmad <shameel2050@gmail.com>
2024-02-23 13:05:56 +0500

    Added validator for post

--- src/router/post/index.js
+++ src/router/post/index.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import PostController from "../../controller/post/index.js";
+import PostValidator from "../../validator/post/index.js";
 const postRoutes = Router();
 postRoutes.get("/post",PostController.getAll);
-postRoutes.post("/post",PostController.create);
+postRoutes.post("/post",PostValidator.create,PostController.create);
 postRoutes.put("/post",PostController.update);
 export default postRoutes;
