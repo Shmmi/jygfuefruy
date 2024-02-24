1c7b9daeb119f6cba7bdb6f6f914efa2f70f3100
Shameel Ahmad <shameel2050@gmail.com>
2024-02-24 14:32:00 +0500

    Added validation for comment model

--- src/router/comment/index.js
+++ src/router/comment/index.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import commentController from "../../controller/comment/index.js";
+import commentValidator from "../../validator/comment/index.js";
 const commentRoute = Router();
-commentRoute.get(commentController.getAll);
-commentRoute.post(commentController.create);
-commentRoute.put(commentController.update);
+commentRoute.get("/comment",commentController.getAll);
+commentRoute.post("/comment",commentValidator.create,commentController.create);
+commentRoute.put("/comment",commentController.update);
 export default commentRoute;
---
+++ src/validator/comment/index.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+const commentValidator = {
+  create: (req,res,next)=>{
+  const schema = Joi.object({
+    content: Joi.string.max(100).required(),
+    userId:  Joi.number(),
+    postId: Joi.number(),
+  });
+  const response = schema.validate(req.body);
+  if(res.error){
+    res.status().json({
+        message: "Invalid data",
+        error:res.error
+    })
+  }
+  console.log(response);
+  next();
+  },  
+}
+export default commentValidator;
